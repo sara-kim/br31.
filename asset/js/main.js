337550bf89234b258e80bfffbbc3cf115c4089b9
minjin <58720123+reference.sara@gmail.com>
2020-01-10 17:14:32 +0900

    0110

--- asset/js/main.js
+++ asset/js/main.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 		var $gobtn = $('.search .searchbar a');
 		$gobtn.on('click',function (e){
 			e.preventDefault();
-			location.href = 'menu/'+tghref;
+			location.href = '/br31./menu/'+tghref;
 		});
 	});
 	//a_history
